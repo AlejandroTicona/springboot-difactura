7afa91c738e757dac4b08471f8c26dbde6db65bf
Aletdev <alejandrortsinani@outlook.com>
2024-11-14 09:27:46 -0400

    Creacion de modelos e inyecciones entre ellos

---
+++ src/main/java/com/ale/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -0,0 +1,43 @@
+package com.ale.curso.springboot.di.factura.springboot_difactura.models;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Invoice {
+    @Autowired
+    private Client client;
+
+    @Value("${invoice.description}")
+    private String descirption;
+    
+    private List<Item> items;
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getDescirption() {
+        return descirption;
+    }
+
+    public void setDescirption(String descirption) {
+        this.descirption = descirption;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+}
